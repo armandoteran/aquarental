0e3297d3ce8803fe8fde579bf235accca0bf53b2
Juan Ceschin <juanceschin@gmail.com>
2020-07-14 20:50:49 -0300

    Add bookings index

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,7 +7,14 @@ class Booking < ApplicationRecord
   validates :state, :start_date, :end_date, :total_price, presence: true
   validate :end_date_after_start_date
 
+  before_validation :set_total_price
+
   def end_date_after_start_date
     errors.add(:end_date, "can't be before start_date") if start_date > end_date
+  end
+
+  def set_total_price
+    days = self.equipment.end_date - self.equipment.start_date
+    self.total_price = self.equipment.price_day * days
   end
 end
