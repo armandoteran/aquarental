49edb259f64ce91cf1efc00599feb16dcb580361
Juan Ceschin <juanceschin@gmail.com>
2020-07-21 21:10:31 -0300

    Corrected typo

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -14,6 +14,6 @@ class BookingPolicy < ApplicationPolicy
   end
 
   def update?
-    record.owner == current_tuser || record.renter == current_user
+    record.owner == current_user || record.renter == current_user
   end
 end
