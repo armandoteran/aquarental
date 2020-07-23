d4e377c991ea45ac83c7d95c27e7a601543bf6db
Armando Teran <ajteranv@gmail.com>
2020-07-22 22:55:04 -0300

    eliminate redundant in show and added before_action :set_booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: %i[show accept reject cancel]
   # Authorization
   skip_after_action :verify_authorized, only: %i[index]
   # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
@@ -14,9 +15,7 @@ class BookingsController < ApplicationController
   end
 
   def show
-    @booking = Booking.find(params[:id])
     authorize @booking
-    @booking = Booking.find(params[:id])
   end
 
   def new
@@ -39,16 +38,8 @@ class BookingsController < ApplicationController
     end
   end
 
-  def edit
-    @booking = Booking.find(params[:id])
-    authorize @booking
-  end
-
-  #def update; end
-
   # Owner only
   def accept
-    @booking = Booking.find(params[:id])
     @booking.state = 'BOOKED'
     @booking.save
     authorize @booking
@@ -56,7 +47,6 @@ class BookingsController < ApplicationController
   end
 
   def reject
-    @booking = Booking.find(params[:id])
     @booking.state = 'REJECTED'
     @booking.save
     authorize @booking
@@ -66,7 +56,6 @@ class BookingsController < ApplicationController
 
   # Renter only
   def cancel
-    @booking = Booking.find(params[:id])
     @booking.state = 'CANCELLED'
     @booking.save
     authorize @booking
@@ -74,14 +63,11 @@ class BookingsController < ApplicationController
   end
   #############
 
-  # def destroy
-    # @booking = Booking.find(params[:id])
-    # @booking.destroy
-    # authorize @booking
-    # redirect_to bookings_path
-  # end
-
   private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
