4d668e2f3cbd6db81268cac3752a2822f4c328ae
Juan Ceschin <juanceschin@gmail.com>
2020-07-23 19:41:28 -0300

    Updated categories

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,5 +1,5 @@
 class Equipment < ApplicationRecord
-  VALID_CATEGORIES = ["Surf", "Canopy"].freeze
+  VALID_CATEGORIES = ["Bote", "Kayak", "Paddle board", "Tablas de surf", "Kite Surf", "Wind surf", "Canoas"].freeze
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   has_many :bookings, dependent: :destroy
 
